684c86ad33c3a3f4889e664537ba8a4a52dcbf81
Mihawk <victra.sonata@gmail.com>
2017-01-21 22:43:32 +0700

    delete unique column user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('nip')->unique();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('password');
             $table->string('role');
             $table->string('jkl');
