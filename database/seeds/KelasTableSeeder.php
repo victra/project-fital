32826a8b8b8e438b04ceaaf43ed5773eaab7c170
latifcah <ifeuselat@gmail.com>
2017-02-12 23:11:07 +0700

    kelas

---
+++ database/seeds/KelasTableSeeder.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class KelasTableSeeder extends Seeder {
+
+	/**
+	 * Auto generated seed file
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		\DB::table('kelas')->delete();
+        
+		\DB::table('users')->insert(array ( 
+			array (
+				'id' => 1,
+				'nama_kelas' => 'X AK 1',
+				'jurusan' => 'Akutansi',
+				'wali_kelas_id' => '1',
+				'created_at' => '2017-01-07 12:00:00',
+				'updated_at' => '2017-01-07 12:01:00',
+				'deleted_at' => NULL,
+			),
+			array (
+				'id' => 1,
+				'nama_kelas' => 'X AK 2',
+				'jurusan' => 'Akutansi',
+				'wali_kelas_id' => '2',
+				'created_at' => '2017-01-07 12:00:00',
+				'updated_at' => '2017-01-07 12:01:00',
+				'deleted_at' => NULL,
+			),
+			array (
+				'id' => 1,
+				'nama_kelas' => 'X AK 3',
+				'jurusan' => 'Akutansi',
+				'wali_kelas_id' => '3',
+				'created_at' => '2017-01-07 12:00:00',
+				'updated_at' => '2017-01-07 12:01:00',
+				'deleted_at' => NULL,
+			),
+			array (
+				'id' => 1,
+				'nama_kelas' => 'X RPL 1',
+				'jurusan' => 'Rekayasa Perangkat Lunak',
+				'wali_kelas_id' => '4',
+				'created_at' => '2017-01-07 12:00:00',
+				'updated_at' => '2017-01-07 12:01:00',
+				'deleted_at' => NULL,
+			),
+			array (
+				'id' => 1,
+				'nama_kelas' => 'X RPL 2',
+				'jurusan' => 'Rekayasa Perangkat Lunak',
+				'wali_kelas_id' => '5',
+				'created_at' => '2017-01-07 12:00:00',
+				'updated_at' => '2017-01-07 12:01:00',
+				'deleted_at' => NULL,
+			),
+			array (
+				'id' => 1,
+				'nama_kelas' => 'X FARMASI',
+				'jurusan' => 'Farmasi',
+				'wali_kelas_id' => '6',
+				'created_at' => '2017-01-07 12:00:00',
+				'updated_at' => '2017-01-07 12:01:00',
+				'deleted_at' => NULL,
+			),
+		));
+	}
+
+}
